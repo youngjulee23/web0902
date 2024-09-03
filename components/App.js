e9b93b28740f0f8c64eb1d5fa07468459463d599
이영주 <sandj0203@naver.com>
2024-09-03 10:12:15 +0900

    Add files via upload

---
+++ components/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import './App.css';
+import AddName from './components/AddName';
+
+
+
+
+function App() {
+    
+
+  return (
+    <>
+    <AddName/>
+    </>
+  );
+}
+
+export default App;
